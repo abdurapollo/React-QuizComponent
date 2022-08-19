b03ec12034a3f50ede4a382ebdcbc4af3b712670
abdurapollo <49436504+abdurapollo@users.noreply.github.com>
2022-08-19 19:35:51 +0530

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,6 +13,10 @@ class Quiz extends Component {
         }
     }
 
+    showNextQuestion(){
+        this.setState({quiz_position: this.state.quiz_position + 1})
+    }
+
     render(){
 
        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
@@ -22,7 +26,10 @@ class Quiz extends Component {
             {
                 isQuizEnd ?
                 <QuizEnd /> :
-                <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}/>
+                <QuizQuestion 
+                   quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}
+                   showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                />
             }
         </div>
        )
